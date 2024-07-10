b50791d3b02c2eae18026c2b8f9a8c234891ea7c
是你李某人 <1677712032@qq.com>
2024-07-10 09:34:56 +0800

    Finish Signage Page
    完成了仪表盘页面

--- entry/src/main/ets/pages/Signage.js
+++ entry/src/main/ets/pages/Signage.js
@@ -1,19 +1,68 @@
-import { WindowFrame } from '../Common/WindowFrame';
-import { HeaderFrame } from '../Common/HeaderFrame';
+import { WindowFrame } from 'ets/Common/WindowFrame';
+import { HeaderFrame } from 'ets/Common/HeaderFrame';
+import { HeaderIcon } from 'ets/Common/HeaderIcon';
+import { getTheme } from 'ets/Common/Theme';
+function styleSolidCard() {
+    
+  
+        .backgroundColor(getTheme().colorScheme.surface)
+        .borderRadius('10vp');
+}
+function styleHeader() {
+    
+  
+        .fontColor(getTheme().colorScheme.onSurface)
+        .fontSize('20fp')
+        .fontWeight(FontWeight.Medium);
+}
+struct StudyProgressCard extends   {
+    constructor() { }
+    build() {
+            .styleSolidCard()
+            .width('100%')
+            .padding('20vp');
+    }
+}
+struct DataPreviewCard extends   {
+    constructor() { }
+    reviewItem(icon, color, title, value, unit) {
+            .alignItems(VerticalAlign.Top)
+            .width('100%');
+    }
+    build() {
+        //概览
+            .width('100%')
+            .styleSolidCard()
+            .padding('20vp');
+    }
+}
+struct CalendarCard extends  {
+    constructor() { }
+    build() {
+            .width('100%')
+            .styleSolidCard()
+            .padding('20vp');
+    }
+}
 struct Signage extends   {
     constructor() { }
+    headerLeftSlot() {
+        HeaderIcon({ src: $r('app.media.icon_keyboard_arrow_down') });
+    }
+    headerRightSlot() {
+        HeaderIcon({ src: $r('app.media.icon_ios_share') });
+    }
     header() {
         HeaderFrame({
-            title: 'THIS IS TITLE'
+            title: '仪表盘',
+            leftSlot: this.headerLeftSlot,
+            rightSlot: this.headerRightSlot
         });
     }
     content() {
-        // 添加子组件
-            .fontSize(20)
-            .backgroundColor(Color.Blue);
-        // 添加子组件
-            .fontSize(20)
-            .backgroundColor(Color.Blue);
+            .width('100%')
+            .alignItems(HorizontalAlign.Start)
+            .padding({ left: '20vp', right: '20vp' });
     }
     build() {
         ;
